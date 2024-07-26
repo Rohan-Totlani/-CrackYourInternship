a4f73cc28f1cc1672c078f5e8935c19b1e358ff9
Rohan Totlani <114063622+Rohan-Totlani@users.noreply.github.com>
2024-07-26 23:44:20 +0530

    Update findDuplicate.cpp

--- arrays/findDuplicate.cpp
+++ arrays/findDuplicate.cpp
@@ -1,1 +1,19 @@
-convert findDupicate.cpp to enable writing
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[nums[0]];//one step 
+        int fast = nums[nums[nums[0]]];//double step
+        while(fast!=slow){
+            //finds first point of intersection in a chained linked list
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+        int n = nums[0];
+        while(n!=slow){
+            //reaches the the number that created the loop in the linked list
+            slow = nums[slow];
+            n = nums[n];
+        }
+        return n;
+    }
+};
